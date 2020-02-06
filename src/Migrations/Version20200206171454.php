e4d32b8a139df946b151be969360a02ccd3e1ac8
kvykhrov <kvykhrov@gmail.com>
2020-02-06 19:18:28 +0200

    migration invoice invoicelines

---
+++ src/Migrations/Version20200206171454.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200206171454 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE invoice_line (id INT AUTO_INCREMENT NOT NULL, comment LONGTEXT NOT NULL, amount INT NOT NULL, quantity NUMERIC(12, 2) NOT NULL, tax NUMERIC(12, 2) NOT NULL, total NUMERIC(12, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE invoice (id INT AUTO_INCREMENT NOT NULL, invoice_line_id INT NOT NULL, invoice_id INT NOT NULL, invoice_date DATE NOT NULL, invoice_number INT NOT NULL, client_id INT NOT NULL, INDEX IDX_90651744BFA24391 (invoice_line_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_90651744BFA24391 FOREIGN KEY (invoice_line_id) REFERENCES invoice_line (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE invoice DROP FOREIGN KEY FK_90651744BFA24391');
+        $this->addSql('DROP TABLE invoice_line');
+        $this->addSql('DROP TABLE invoice');
+    }
+}
